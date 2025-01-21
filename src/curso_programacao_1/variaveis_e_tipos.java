fe4ff36e0dede10c98ad4ada9f012097bce2dc54
Joao Gabriel <joao.gcs314@gmail.com>
2025-01-20 19:48:58 -0500

    Aula 1

---
+++ src/curso_programacao_1/variaveis_e_tipos.java
@@ -0,0 +1,33 @@
+package curso_programacao_1;
+
+public class variaveis_e_tipos {
+
+	public static void main(String[] args) {
+		
+		// Tipos primitivos em Java
+		
+		// 1 bit = 2 "possibilidades" 
+		// byte = 8 bits
+		// short = 16 bits
+		// int = 32 bits
+		// long = 64 bits
+		
+		// Pontos flutuantes 
+		// float = 32 bits
+		// double = 64 bits ( Geralmente usado para números quebrados)
+		
+		// Um cacactere Unicode 
+		// char = 16 bits - Possivel verificar a unicode table no seguinte site 
+		// unicode-table.com
+		
+		// valor verdade = boolean
+		
+		byte teste = 4;
+		byte teste_2= 2;
+		float div;
+		
+		div = (teste/teste_2);
+		System.out.println(div);
+	}
+
+}
