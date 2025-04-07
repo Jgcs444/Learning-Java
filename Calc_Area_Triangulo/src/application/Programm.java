fc4dd7ad6b087934bddf7ad4a5d5a22036904d2e
Joao Gabriel <joao.gcs314@gmail.com>
2025-04-07 17:39:27 -0400

    Subindo ajustes do exercicio relacionado aos trinagulos

--- Calc_Area_Triangulo/src/application/Programm.java
+++ Calc_Area_Triangulo/src/application/Programm.java
@@ -8,37 +8,20 @@ public class Programm {
 
 	public static void main(String [] args) {
 		
-		Triangle x,y;
+		
 		Scanner sc = new Scanner(System.in);
 		double areaX,areaY;
-		
-		x = new Triangle();
-		y = new Triangle();
+		Triangle x = Text.lerTriangulo(sc, "X");
+		Triangle y = Text.lerTriangulo(sc, "Y");;
 		
 		// Instanciado um obj do tipo Triangle em uma área de memória chamada heap, onde são criados objs dinâmicos, durante 
 		// a execução.
 		
-		System.out.print("\nOlá, por gentileza informe um número para o lado A do primeiro triangulo : \n ");
-		x.a = sc.nextDouble();
-		System.out.print("\nOlá, por gentileza informe um número para o lado B do primeiro triangulo: \n ");
-		x.b = sc.nextDouble();
-		System.out.print("\nOlá, por gentileza informe um número para o lado C do primeiro triangulo : \n ");
-		x.c = sc.nextDouble();
-		
-		sc.nextLine();
-		
-		System.out.println("\nOlá, por gentileza informe um número para o lado A: ");
-		y.a = sc.nextDouble();
-		System.out.println("\nOlá, por gentileza informe um número para o lado B: ");
-		y.b = sc.nextDouble();
-		System.out.println("\nOlá, por gentileza informe um número para o lado C: ");
-		y.c = sc.nextDouble();
-		
 		areaX = x.area();
 		areaY = y.area();
 		
-		System.out.printf("O valor da área do Triãngulo x é %.2f: ", areaX);
-		System.out.printf("\nO valor da área do Triãngulo y é %.2f: ", areaY);
+		System.out.printf("O valor da área do Triãngulo x é: %.2f ", areaX);
+		System.out.printf("\nO valor da área do Triãngulo y é: %.2f ", areaY);
 
 		sc.close();
 	}
--- curso_programacao/src/aplication/Text.java
+++ curso_programacao/src/aplication/Text.java
@@ -6,14 +6,14 @@ import entities.Triangle;
 
 public class Text {
 	
-	public static Triangle lerTriangulo ( Scanner sc, String valor) {
+	public static Triangle lerTriangulo ( Scanner sc, String nome) {
 		Triangle t = new Triangle();
 		
-		System.out.printf("Olá, por gentileza informe o valor A do triângulo %s:", valor);
+		System.out.printf("Olá, por gentileza informe o valor A do triângulo %s:", nome);
 		t.a = sc.nextDouble();
-		System.out.printf("Olá, por gentileza informe o valor B do triângulo %s:", valor);
+		System.out.printf("Olá, por gentileza informe o valor B do triângulo %s:", nome);
 		t.b = sc.nextDouble();
-		System.out.printf("Olá, por gentileza informe o valor C do triângulo %s:", valor);
+		System.out.printf("Olá, por gentileza informe o valor C do triângulo %s:", nome);
 		t.c = sc.nextDouble();
 		
 		return t;
