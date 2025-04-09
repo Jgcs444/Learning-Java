2eab239353afe17807e2b8ef99a55fc670831e27
Joao Gabriel <joao.gcs314@gmail.com>
2025-04-08 21:59:51 -0400

    - Today i've studied more OOP, doing an exercise related to a market. Missing points : Add the option to remove the quantity of products.

--- Mercado/src/entities/Product.java
+++ Mercado/src/entities/Product.java
@@ -1,10 +1,21 @@
 package entities;
 
 public class Product {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public String name;
+	public double price;
+	public int quantity;
+	
+	
+	public double valor_T () {
+		return price*quantity;
 	}
+	
+	public void addNewQuantity (int adicionalQuantity) {
+		
+		 this.quantity += adicionalQuantity;
+		
+	}
+	
 
 }
